7af9b46eb334fc9640eea7db4db004b9ba1d5cab
zhqingyan-1 <angelalico814@gmail.com>
2026-06-06 18:04:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function openLetter(){
-    document.getElementById("envelope-section")
-        .style.display = "none";
 
-    document.getElementById("letter-section")
-        .classList.remove("hidden");
-}
-
-function startFireworks(){
-    alert("🎆 Imagine beautiful fireworks here!");
-}
